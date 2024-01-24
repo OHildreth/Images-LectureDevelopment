ef36c976f985575cd74ad37e9bb768dbc7f8d1ca
Owen Hildreth <ohildreth@mines.edu>
2024-01-24 13:33:40 -0700

    Initial Commit

---
+++ Images-LectureDevelopment/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Images-LectureDevelopment
+//
+//  Created by Owen Hildreth on 1/24/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
